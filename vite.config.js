43131d665e21caee13e4a4dd5f22d70810b63a23
Adish Sagarawat <adish.sag@gmail.com>
2026-06-18 14:39:17 +0530

    refactor: migrate project structure to source folders with vite and update data source

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    minify: 'esbuild',
+    sourcemap: false,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
